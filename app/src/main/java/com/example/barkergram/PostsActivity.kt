b3ba4d8543cd7628ce735fef827a8b577c2c7e5b
dsteed112 <dsteed112@gmail.com>
2020-06-30 10:05:10 -0600

    set up query to firebase

--- app/src/main/java/com/example/barkergram/PostsActivity.kt
+++ app/src/main/java/com/example/barkergram/PostsActivity.kt
@@ -1,12 +1,15 @@
 package com.example.barkergram
 
+import android.app.DownloadManager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.example.barkergram.models.Post
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.core.Query
 import com.google.firebase.firestore.proto.TargetGlobal
 
 private const val TAG = "PostsActivity"
@@ -21,21 +24,28 @@ class PostsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_posts)
 
 
+
+
+
         firestoreDb = FirebaseFirestore.getInstance()
 
-        val postsReference = firestoreDb.collection("posts")
+        val postsReference = firestoreDb
+            .collection("posts")
+            .limit(20)
+            .orderBy("creation_time_ms", com.google.firebase.firestore.Query.Direction.DESCENDING)
         postsReference.addSnapshotListener{ snapshot, exception ->
             if (exception != null || snapshot == null) {
                 Log.e(TAG, "Exception when querying posts", exception)
                 return@addSnapshotListener
             }
-            for (document in snapshot.documents){
-                Log.i(TAG, "Document ${document.id}: ${document.data}")
+
+            val postList = snapshot.toObjects(Post::class.java)
+
+            for (post in postList){
+                Log.i(TAG, "Post: $post")
 
             }
         }
-
-        //query to firestore to get data
 
     }
 
---
+++ app/src/main/java/com/example/barkergram/models/User.kt
@@ -0,0 +1,5 @@
+package com.example.barkergram.models
+
+data class User(var username: String = "")
+
+//                var age: Int = 0)
